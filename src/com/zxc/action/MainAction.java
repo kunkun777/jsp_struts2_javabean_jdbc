a3024226bfe8fcd8051bf4a51d64f2cbd9b61b27
stillexist <56880492+stillexist@users.noreply.github.com>
2020-10-31 17:54:42 +0800

    垃圾代码10.31

--- src/com/zxc/action/MainAction.java
+++ src/com/zxc/action/MainAction.java
@@ -1,68 +1,69 @@
 package com.zxc.action;
-import java.sql.*;
-import java.util.*;
-import com.zxc.model.vo.*;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
 import com.zxc.jdbc.SqlSrvDBConn;
-import com.opensymphony.xwork2.*;
-public class MainAction extends ActionSupport{
-	private UserTable user;
-	//???????????? execute ????
-	public String execute() throws Exception{
-		String usr=user.getUsername();			//????????????
-		String pwd=user.getPassword();			//???????????
-		boolean validated=false;				//?????????
-		SqlSrvDBConn sqlsrvdb=new SqlSrvDBConn();
-		ActionContext context=ActionContext.getContext();
-		Map session=context.getSession();		//???????????????????????????? 
-		UserTable user1=null;
-		//???? UserTable ????????????η?????????????????????????????????????????Σ????????????????????????????????????
-		user1=(UserTable)session.get("user");
-		//???????????ν?????????δ?? user1 ?????????? null
-		if(user1==null){
-			//??? userTable ???е???
-    		String sql="select * from userTable";
-    		ResultSet rs=sqlsrvdb.executeQuery(sql);		//???????
-    		try {
-				while(rs.next())
-				{
-					if((rs.getString("username").trim().compareTo(usr)==0)&&(rs.getString("password").compareTo(pwd)==0)){
-						user1=new UserTable();				//?????????? JavaBean ???? user1
-						user1.setId(rs.getInt(1));
-						user1.setUsername(rs.getString(2));
-						user1.setPassword(rs.getString(3));
-						session.put("user", user1);			//?? user1 ??????????
-						validated=true;						//???? true ????????????
-					}
-				}
-	    		rs.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-    		sqlsrvdb.closeStmt();
-    		sqlsrvdb.closeConn();
-		}
-        else{
-        	validated=true;
+import com.zxc.model.vo.UserTable;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class MainAction extends ActionSupport {
+    private UserTable user;
+
+    public String execute() {
+        String usr = user.getUsername();
+        String pwd = user.getPassword();
+        boolean validated = false;
+        SqlSrvDBConn sqlsrvdb = new SqlSrvDBConn();
+        ActionContext context = ActionContext.getContext();
+        Map session = context.getSession();
+        UserTable user1 = null;
+
+        user1 = (UserTable) session.get("user");
+
+        if (user1 == null) {
+            String sql = "select * from userTable";
+            ResultSet rs = sqlsrvdb.executeQuery(sql);
+            try {
+                while (rs.next()) {
+                    if ((rs.getString("username").trim().compareTo(usr) == 0) && (rs.getString("password").compareTo(pwd) == 0)) {
+                        user1 = new UserTable();
+                        user1.setId(rs.getInt(1));
+                        user1.setUsername(rs.getString(2));
+                        user1.setPassword(rs.getString(3));
+                        session.put("user", user1);
+                        validated = true;
+                    }
+                }
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            sqlsrvdb.closeStmt();
+            sqlsrvdb.closeConn();
+        } else {
+            validated = true;
         }
-        if(validated)
-        {
-        	//???????????????"success"
-        	return "success";
+        if (validated) {
+            return "success";
+        } else {
+            return "error";
         }
-        else{
-            //??????????????"error"
-        	return "error";
-        }
-		
-	}
-	public  void validate(){
-		if (user.getUsername()==null||user.getUsername().trim().equals(""))
-			addFieldError("user.username","用户名必须填写！");
-	}
-	public UserTable getUser(){
-		return user;
-	}
-	public void setUser(UserTable user){
-		this.user=user;
-	}
+
+    }
+
+    public void validate() {
+        if (user.getUsername() == null || user.getUsername().trim().equals(""))
+            addFieldError("user.username", "用户名必须填写！");
+    }
+
+    public UserTable getUser() {
+        return user;
+    }
+
+    public void setUser(UserTable user) {
+        this.user = user;
+    }
 }
